f956f3d4c9eba761e4c9647c41d16faa6a0bae62
Ravi <dawaderavi4704@gmail.com>
2021-09-05 08:42:16 +0530

    final

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn hospital.wsgi --log-file -
+web: gunicorn hospital.wsgi
--- hospital/settings.py
+++ hospital/settings.py
@@ -48,9 +48,12 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'hospital.urls'
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 TEMPLATES = [
     {
@@ -119,9 +122,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static")
+    os.path.join(BASE_DIR, "static"),
 ]
 
 LOGOUT_REDIRECT_URL = '/'
